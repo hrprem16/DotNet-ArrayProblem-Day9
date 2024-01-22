6e3b029fe40dfb298bec1b7b34342967e7831202
hrprem16 <hrprem16@gmail.com>
2024-01-23 04:07:46 +0530

    [Prince] Add: Added Print the star pattern

--- ArrayProblems/Program.cs
+++ ArrayProblems/Program.cs
@@ -8,29 +8,16 @@ namespace findDuplicate
 
         static void Main()
         {
-
-            int[] arr = { 10, 7, 12, 14,17, 2,  15};
-            int min = arr[0]; //Assume the 0th index value is minimum
-            int max = arr[5]; //Assume the last index value is maximum
-            for (int i = 0; i < arr.Length; i++)
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < n; i++)
             {
-                if (arr[i] < min)
+                for(int j = 0; j < n-i; j++)
                 {
-                    min = arr[i];
+                    Console.Write("*");
                 }
-              
+                Console.WriteLine();
             }
-            for (int j = 0; j < arr.Length; j++)
-            {
-                if (arr[j] > max)
-                {
-                    max = arr[j];
-                }
-
-            }
-            Console.WriteLine($"The Minimum element in the given array is {min}");
-            Console.WriteLine($"The Minimum element in the given array is {max}");
-
+            
         }
     }
 }
