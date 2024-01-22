9924371b35a4b942fab288b51543fd2bac58c8da
hrprem16 <hrprem16@gmail.com>
2024-01-23 02:40:57 +0530

    [Prince] Add: Added find the total number of duplicate elements

---
+++ ArrayProblems/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace findDuplicate
+{
+
+    class Program
+    {
+
+
+        static void Main()
+        {
+            int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
+
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+
+
+                    }
+                }
+            }
+            Console.WriteLine($"Total number of duplicate element in the give arrays are : {count}");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
