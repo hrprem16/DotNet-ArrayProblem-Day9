d60ad6806f42c3ecfb6604eacf9bfa6e66499c66
hrprem16 <hrprem16@gmail.com>
2024-01-23 04:28:35 +0530

    [Prince] Add: Added Print the  vertical rectangle star pattern

--- ArrayProblems/Program.cs
+++ ArrayProblems/Program.cs
@@ -1,23 +1,34 @@
 ﻿using System;
-namespace findDuplicate
-{
 
+namespace FindDuplicate
+{
     class Program
     {
-
-
         static void Main()
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i < n; i++)
+
+            for (int i = 0; i < n; i++)
             {
-                for(int j = 0; j < n-i; j++)
+                if (i == 0 || i == n - 1)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                else
                 {
                     Console.Write("*");
+                    for (int space = 0; space < n - 2; space++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("*");
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
             }
-            
         }
     }
 }
