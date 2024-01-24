6d1780cde624c50a9cde6b71d0b5646e647201b7
Prince Bhagat <83371230+hrprem16@users.noreply.github.com>
2024-01-24 14:10:00 +0530

    Update Program.cs

--- ArrayProblems/Program.cs
+++ ArrayProblems/Program.cs
@@ -1,74 +1,36 @@
-﻿using System;
-namespace findDuplicate
-{
+using System;
 
+namespace CountElementFrequency
+{
     class Program
     {
-
-
         static void Main()
         {
-            
             int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
+
             for (int i = 0; i < arr.Length; i++)
             {
-                int j;
-
-                for (j = 0; j < i; j++)
+                // Skip if the element is already processed
+                if (arr[i] == -1)
                 {
+                    continue;
+                }
 
+                int count = 1;
+
+                // Count the frequency of arr[i]
+                for (int j = i + 1; j < arr.Length; j++)
+                {
                     if (arr[i] == arr[j])
                     {
-                        break;
-
+                        count++;
+                        // Mark the repeated element as processed to avoid recounting
+                        arr[j] = -1;
                     }
-
                 }
-                if (i == j) Console.Write(arr[i] + " ");
 
-
+                Console.WriteLine($"{arr[i]}: {count}");
             }
-
-
-
-
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
