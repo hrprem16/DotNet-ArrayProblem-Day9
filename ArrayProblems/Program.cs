1e26aa27dc170aa6664d1512d9bb176ac12436b8
Prince Bhagat <83371230+hrprem16@users.noreply.github.com>
2024-01-24 14:12:30 +0530

    Update Program.cs

--- ArrayProblems/Program.cs
+++ ArrayProblems/Program.cs
@@ -8,28 +8,19 @@ namespace CountElementFrequency
         {
             int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                // Skip if the element is already processed
-                if (arr[i] == -1)
-                {
+            for(int i=0;i<arr.Length;i++){
+                if(arr[i]==-1){
                     continue;
+                    
                 }
-
-                int count = 1;
-
-                // Count the frequency of arr[i]
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] == arr[j])
-                    {
+                int count =1;
+                for(int j=i+1;j<arr.Length;j++){
+                    if(arr[i]==arr[j]){
                         count++;
-                        // Mark the repeated element as processed to avoid recounting
-                        arr[j] = -1;
+                        arr[j]=-1;
                     }
                 }
-
-                Console.WriteLine($"{arr[i]}: {count}");
+                Console.WriteLine($"Occurence of {arr[i]} is : {count}");
             }
         }
     }
